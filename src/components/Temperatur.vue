1355ac450bdf499bfde7fb1d7f67cb43c8785db9
jrpikong <jr.pikong@gmail.com>
2017-11-16 12:11:43 +0700

    first commit,crate Temperatur file and install Vuex

---
+++ src/components/Temperatur.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <select v-model="city">
+      <option value="Jakarta">Jakarta</option>
+      <option value="Tokyo">Tokyo</option>
+      <option value="London">London</option>
+    </select>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        city:''
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export const getApiID = function () {
+  return 'APPID=481e3bc28e5264e5607c2b65b449bfc1'
+}
